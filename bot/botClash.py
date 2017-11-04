b255dc7f597e6f35cd4b867f2adf035c903f5a83
MagicJHC10 <juancesc96@gmail.com>
2017-11-04 20:11:51 +0100

    Subo nueva funcion del bot

--- bot/botClash.py
+++ bot/botClash.py
@@ -3,6 +3,8 @@ from telebot import types
 from telebot import util
 import time
 import os
+import scrapeo
+
 bot = telebot.TeleBot("474881369:AAEZ8tf53Su3TUSnPeaJ2k4IuPPavtWLXGo")
 
 
@@ -19,5 +21,12 @@ def hola_mundo(update):
    	mensaje= "Hola jugador de clash royale"
 	bot.send_message( chat_id, mensaje)
 
+
+bot.set_update_listener(listener)
+@bot.message_handler(commands=['listacartas']) 
+def listadocartas(update):
+	chat_id = update.chat.id
+	mensaje= scrapeo.mostrarcartas()
+	bot.send_message( chat_id, mensaje)
 
 bot.polling(none_stop=True)
