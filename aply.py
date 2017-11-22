09953d4a7fd506dacdd56408af229c182fd1b024
MagicJHC10 <juancesc96@gmail.com>
2017-11-22 11:21:11 +0100

    Arreglo aply.py Fixes #13

--- aply.py
+++ aply.py
@@ -1,19 +1,19 @@
 from flask import Flask, request, jsonify
 import json
-from flask_restful import Resource, Api
+import os
 
 
 app = Flask(__name__)
 
-{
-   "status": "OK"
-}
+
 
 @app.route("/")
-def func():
-    data = {"status": "OK"}
-    return json.dumps(data)
+def rutaStatus():
+    return jsonify(status='OK')
+
+@app.route("/status")
+def rutaStatusDocker():
+    return jsonify(status='OK')
 
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug = True, use_reloader = True)
+    app.run(debug = True, use_reloader = True)
